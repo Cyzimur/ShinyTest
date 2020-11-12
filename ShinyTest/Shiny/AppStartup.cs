a6536fe01dea88252a492b62d6d27bd8061f2976
Charles Christian <>
2020-11-12 13:12:26 +0000

    Adding shiny stuff

---
+++ ShinyTest/Shiny/AppStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny;
+using Shiny.Logging;
+using Shiny.Prism;
+using Prism.DryIoc;
+
+namespace ShinyTest.Shiny
+{
+    public class AppStartup : PrismStartup
+    {
+        public AppStartup() : base(PrismContainerExtension.Current)
+        {
+        }
+
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            Log.UseConsole();
+            Log.UseDebug();
+
+            //services.UsePushAzureNotificationHubs<PushDelegate>("Your listener connection string", "your hub name");
+            services.UseNotifications(true);
+
+            //Startup Task
+            services.AddSingleton<ServiceStack>();
+        }
+    }
+}
---
+++ ShinyTest/Shiny/ServiceStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Prism.Ioc;
+using Shiny;
+using Shiny.Notifications;
+
+namespace ShinyTest.Shiny
+{
+    public class ServiceStack : IShinyStartupTask
+    {
+        public ServiceStack(IContainerExtension container, INotificationManager notificationManager)
+        {
+            Container = container;
+            NotificationManager = notificationManager;
+        }
+
+        public IContainerExtension Container { get; }
+        public INotificationManager NotificationManager { get; }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterRequiredTypes();
+        }
+
+        public void Start()
+        {
+            RegisterTypes(Container);
+            NotificationManager.Send("ShinyTest", "App Started!");
+        }
+    }
+}
